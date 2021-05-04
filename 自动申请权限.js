4b15b7bf5cb0d34d2e267e4f3de4390f48fba1e5
SMCYDS <62012380+SMCYDS@users.noreply.github.com>
2021-05-04 08:45:28 +0800

    Create 自动申请权限.js

---
+++ 自动申请权限.js
@@ -0,0 +1,14 @@
+//进入地图的封面后在F12里面的控制台运行
+(async function(){
+    try{
+    document.querySelector("._2kWmDVfvQZvUKLSQONkaIr._26j0LORdVu56VuHym8I082._6z5cYn8V_bEzjprGBfMk2").click();
+    }catch(e){}
+    for(let i=0;i<5;i++)
+    setTimeout(()=>{
+        try{
+            document.querySelector("._2YHbfjhyO9R33GYylhWdje").click();
+        }catch(e){};
+    },1000)
+    
+    
+})()
