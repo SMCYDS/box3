8ef7992dacd0e418b9d2b46cd2a40c3f47319134
SMCYDS <62012380+SMCYDS@users.noreply.github.com>
2021-05-04 14:36:47 +0800

    Update trackBlocks.js

--- trackBlocks.js
+++ trackBlocks.js
@@ -1,23 +1,24 @@
 //跟踪地图里的所有方块数量
 (async function(){
-var total=0;
+    var blocks=[];
+    for(let i=0;i<440;i++){
+        blocks[i]=0;
+    }
+    
     for(let i=0;i<=126;i++){
         for(let j=0;j<=126;j++){
             for(let k=0;k<=126;k++){
                 if(voxels.getVoxel(i,k,j)!=0){
-	total++;
+                    blocks[voxels.getVoxel(i,k,j)]+=1;
                 }
                 
             }
         }
     }
-
-world.say("============");
-var types=0;
-if(total!=lastTotal){
-	status="有变化，多了"+(total-lastTotal)+"个方块"
-}else{status="无变化"}
-world.say("方块数据总结\n方块数据"+status+"\n方块总数："+total+"\n人造方块总数："+(total-16129-16129-112903));
-var lastTotal=total;
-
+    for(let i=1;i<blocks.length;i++){
+        if(blocks[i]!=0){
+            world.say(voxels.name(i)+" 总共有"+blocks[i]+"个")
+        }
+        
+    }
 })()
